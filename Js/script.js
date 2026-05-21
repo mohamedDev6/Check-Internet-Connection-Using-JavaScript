ad9b5ad712f48142ad871ecfb84d7862e13c8b2d
mohamedDev6 <mohamedeslam6264@gmail.com>
2026-05-21 18:57:35 +0300

    second commit

--- Js/script.js
+++ Js/script.js
@@ -3,14 +3,14 @@ let ul = document.querySelector("ul");
 let reloadBtn = document.querySelector(".reload-btn");
 
 function online() {
-    internetStatus.textContent = "You Are Online Now";
+    internetStatus.textContent = "You are online now";
     internetStatus.style.color = "green";
     ul.classList.add("hide");
     reloadBtn.classList.add("hide");
 }
 
 function offline() {
-    internetStatus.textContent = "You Are Offline Now";
+    internetStatus.textContent = "You are offline now";
     internetStatus.style.color = "#666";
     ul.classList.remove("hide");
     reloadBtn.classList.remove("hide");
